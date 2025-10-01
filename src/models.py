a17ec4396a3ab8244ce589c3c88f33b6dde875da
Rejoy Thomas Mathew <84540746+rejoythomasmathew@users.noreply.github.com>
2025-10-01 15:10:39 +0400

    perf: :zap: improve transcription speed by setting language="en" for WhisperX model

--- src/models.py
+++ src/models.py
@@ -2,7 +2,7 @@ import whisperx
 import spacy
 import medspacy
 
-transcription_model = whisperx.load_model("large-v2", device="cpu", compute_type="int8")
+transcription_model = whisperx.load_model("large-v2", language="en", device="cpu", compute_type="int8")
 
 nlp_model = spacy.load("en_core_web_sm", disable=["ner", "parser"])
 nlp_model = medspacy.load(nlp_model, medspacy_enable=["medspacy_sectionizer"], load_rules=False)
